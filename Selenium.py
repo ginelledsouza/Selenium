1c008104d70b7e9d52eb5dcb0bebc92fb462a402
Ginelle D'souza <64312327+ginelledsouza@users.noreply.github.com>
2022-02-10 20:42:54 +0530

    Update Selenium.py

--- Selenium.py
+++ Selenium.py
@@ -27,12 +27,12 @@ driver.find_element_by_xpath('//*[@id="pass"]').send_keys(password)
 driver.find_element_by_xpath('//*[@id="loginbutton"]').click()
 
 driver.implicitly_wait(15)
-driver.find_element_by_xpath('//*[@id="mount_0_0_GE"]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[1]/div/div/div[1]/div/div/div[1]/div[1]/ul/li[1]/div/a/div[1]/div[2]/div/div/div/div/span/span').click()
-driver.find_element_by_xpath('//*[@id="mount_0_0_GE"]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[2]/div/div/div[4]/a/div[1]/div[2]/div[1]/div/div/div/span/span').click()
+driver.find_element_by_xpath('//*[@id="mount_0_0_vE"]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[1]/div/div/div[1]/div/div/div[1]/div[1]/ul/li[1]/div/a/div[1]/div[2]/div/div/div/div/span/span').click()
+driver.find_element_by_xpath('//*[@id="mount_0_0_vE"]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[2]/div/div/div[4]/a/div[1]').click()
 
-friends = int(driver.find_element_by_xpath('//*[@id="mount_0_0_GE"]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[2]/div/div[3]/div/div/div/div/div/h2/span/span').text.split("friends")[0].strip())
+friends = int(driver.find_element_by_xpath('//*[@id="mount_0_0_vE"]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[2]/div/div[3]/div/div/div/div/div/h2/span/span').text.split("friends")[0].strip())
 
-element = driver.find_element_by_xpath('//*[@id="mount_0_0_GE"]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[2]/div')
+element = driver.find_element_by_xpath('//*[@id="mount_0_0_vE"]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[1]')
 
 friendsdata = element.get_attribute('outerHTML')
 
@@ -41,6 +41,7 @@ s = BeautifulSoup(friendsdata, 'html.parser')
 # Extract Friends Profile Link
 friendsList = s.find_all('a')
 friendsList = [pt['href'] for pt in friendsList]
+friendsList = [pt for pt in friendsList if len(pt)>20]
 
 # Extract Friends Name
 friendsName = s.find_all('span',{'class':'d2edcug0 hpfvmrgz qv66sw1b c1et5uql lr9zc1uh jq4qci2q a3bd9o3v lrazzd5p oo9gr5id'})
